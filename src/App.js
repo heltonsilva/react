b6319800a540d8c6cf66e749cd983d6142380647
heltonsilva <heltonsilva.trab@.com>
2020-11-26 13:36:11 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Results from './Results';
+import requests from './requests';
+
+function App() {
+
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTrending);
+
+  return (
+    <div className="App">
+
+      {/* Header */}
+      <Header/>
+
+      {/* Nav */}
+      <Nav setSelectedOption={setSelectedOption}/>
+
+      {/* Results */}
+      <Results selectedOption={selectedOption}/>
+    </div>
+  );
+}
+
+export default App;
